58aceae66d57c2d58497792a8001b369224cedb2
cheetosfan4 <146988149+cheetosfan4@users.noreply.github.com>
2025-11-10 12:52:01 -0500

    player bar movement

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,41 @@
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+    public float playerY;
+    public float playerWidth;
+    public float borderWidth;
+
+    private Rigidbody2D rb;
+    private Vector3 mouseScreenPosition;
+    private Vector3 mousePosition;
+    private Vector3 targetPosition;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        targetPosition = new Vector3(0, playerY, 0);
+        rb.position = targetPosition;
+    }
+
+    void Update() {
+        mouseScreenPosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
+        //12.8 is half the size of background, will not change
+        //might change border width or player width
+        //could also get player width from collision box when it is added
+        float boundaryPos = 12.8f - borderWidth - (playerWidth / 2);
+
+        if (mousePosition.x > -boundaryPos && mousePosition.x < boundaryPos) {
+            targetPosition.x = mousePosition.x;
+        }
+        else if (mousePosition.x < 0) {
+            targetPosition.x = -boundaryPos;
+        }
+        else if (mousePosition.x > 0) {
+            targetPosition.x = boundaryPos;
+        }
+
+        rb.position = targetPosition;
+    }
+}
