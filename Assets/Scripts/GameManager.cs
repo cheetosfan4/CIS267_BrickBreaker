5b83c10a69784ce8c72887594375bfc46f961e4c
cheetosfan4 <146988149+cheetosfan4@users.noreply.github.com>
2025-12-14 13:25:46 -0500

    added comment header to game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,9 @@
+//====================================================================================================
+//Author        :       Marc McLennan
+//Date          :       12-14-2025
+//Description   :       CIS267 Homework #2; Brick Breaker
+//====================================================================================================
+
 using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
 using TMPro;
